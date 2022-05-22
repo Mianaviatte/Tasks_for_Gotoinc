9f362757f90d4a2c771fcf86e1bdbe0bab5cf2d0
Mianaviatte <52130570+Mianaviatte@users.noreply.github.com>
2022-05-23 00:14:00 +0300

    Initial commit

---
+++ task2_filtered_text_array.py
@@ -0,0 +1,38 @@
+# Write a method / function that accepts text (take into account punctuation and special characters) 
+# and returns an array of the three most common words in the text in descending order.
+# A word is a string of letters (A to Z) that optionally contains one or more apostrophes (')
+# Matches must not be case sensitive and words in the returned array must be lowercase
+# If the text contains less than three unique words, return an empty array.
+
+from collections import Counter
+from operator import itemgetter
+import re
+
+# Input with lorem ipsum document opened for reading
+with open('lorem_ipsum.txt') as text_sample:
+    texts = text_sample.read()
+
+    if texts is None:
+        texts = input('Please, provide a test sample.')
+
+    # Lowering case & filtering out numbers, punctuation and special characters except apostrophes (')
+    re.sub(r'[A-Za-zÀ-ÿ \']', '', texts)
+    texts = re.sub(r'[-()\"#/@;:<>{}`+=~|.!?,\n]', '', texts.lower())
+    
+    # Splitting word by word and saving as a list.
+    words = texts.split(' ')
+
+    # Collect a dict with keys = common words and values = amount of them on text, then order by amount
+    lorem_count = {word: words.count(word) for word in set(words)}
+    sorted_lorem_count = sorted(lorem_count.items(), key=itemgetter(1), reverse=True)
+    
+    # If dict is less then 3 items long, return an empty array. 
+    # If not - return a list of keys, where values are greatest.
+    results = [] if len(lorem_count)<3 else [*sorted_lorem_count]
+    results = results[:3]
+
+    # ALTERNATIVE TO LINES 25-32:
+    # results = [] if len(set(words))<3 else Counter(words).most_common(3)
+
+print (sorted_lorem_count)
+print (results)
